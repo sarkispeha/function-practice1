7327e40aed0cef31e1814a7c504e5fbd93fde40a
Sarkis Peha <sarkispeha@gmail.com>
2014-09-16 12:01:20 -0600

    practicing doing commits more often, at problem 6

---
+++ main.js
@@ -0,0 +1,27 @@
+var tripleFive = function() {
+	for(var i = 0; i < 3; i++){
+		console.log('Five!');
+	};
+}
+
+var lastLetter = function (str) {
+	console.log(str[str.length-1]);
+}
+
+var square = function (num) {
+	console.log(num * num);
+}
+
+var negate = function (num) {
+	if(num <0){
+		console.log(Math.abs(num));
+	}else{
+		console.log(0 - num);
+	};
+}
+
+var toArray = function (a,b,c) {
+	var Arr = [];
+	Arr.push(a,b,c);
+	console.log(Arr);
+}
